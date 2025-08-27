63ec4943c6d0a823b5c11e6d205270b034e7483c
Trevor L. McDonell <trevor@passivelogic.com>
2025-08-27 15:32:26 +0200

    feat: clean up interposition implementation

--- Sources/tracy-cbits/tracy-interpose.c
+++ Sources/tracy-cbits/tracy-interpose.c
@@ -7,7 +7,9 @@
 
 #ifdef TRACY_ENABLE
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE // required for RTLD_NEXT
+#endif
 
 // Not including our custom header because SPM is not applying preprocessor
 // defines when including the header only (so we need to #define TRACY_ENABLE
@@ -22,43 +24,47 @@
 /* #include "tracy-cbits.h" */
 
 #include "tracy/public/tracy/TracyC.h"
-// #include "tracy/public/tracy/Tracy.hpp"
 
 #include <assert.h>
 #include <dlfcn.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-#if defined(__GNUC__) || defined(__clang__)
-#define tracy_unlikely(x)     (__builtin_expect(!!(x),false))
-#define tracy_likely(x)       (__builtin_expect(!!(x),true))
-#elif (defined(__cplusplus) && (__cplusplus >= 202002L)) || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L)
-#define tracy_unlikely(x)     (x) [[unlikely]]
-#define tracy_likely(x)       (x) [[likely]]
+#if defined(__APPLE__)
+#include <malloc/malloc.h>
 #else
-#define tracy_unlikely(x)     (x)
-#define tracy_likely(x)       (x)
+#include <malloc.h>
 #endif
 
 #if defined(__GNUC__) || defined(__clang__)
+#define TRACY_UNLIKELY(x)     (__builtin_expect(!!(x),false))
+#define TRACY_LIKELY(x)       (__builtin_expect(!!(x),true))
+#elif (defined(__cplusplus) && (__cplusplus >= 202002L)) || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L)
+#define TRACY_UNLIKELY(x)     (x) [[unlikely]]
+#define TRACY_LIKELY(x)       (x) [[likely]]
+#else
+#define TRACY_UNLIKELY(x)     (x)
+#define TRACY_LIKELY(x)       (x)
 #endif
 
+#define DLSYM_REAL(NAME) \
+  static __typeof__(NAME)* real_##NAME = NULL; \
+  if TRACY_UNLIKELY(!real_##NAME) { \
+    *(void**) &real_##NAME = dlsym(RTLD_NEXT, #NAME); \
+    assert(real_##NAME  != NULL && "dlsym failed"); \
+  }
 
 
 static void* tracy_malloc(size_t size)
 {
-  static void* (*next)(size_t) = NULL;
-  if tracy_unlikely(!next) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "malloc");
-    assert(next != NULL);
-    assert(next != &tracy_malloc);
-  }
+  DLSYM_REAL(malloc);
 
-  void* ptr = next(size);
+  void* ptr = real_malloc(size);
   if (ptr == NULL)
     return NULL;
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCAlloc(ptr, size);
   }
 
@@ -67,18 +73,13 @@ static void* tracy_malloc(size_t size)
 
 static void* tracy_calloc(size_t count, size_t size)
 {
-  static void* (*next)(size_t, size_t) = NULL;
-  if tracy_unlikely(!next) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "calloc");
-    assert(next != NULL);
-    assert(next != &tracy_calloc);
-  }
+  DLSYM_REAL(calloc);
 
-  void* ptr = next(count, size);
+  void* ptr = real_calloc(count, size);
   if (ptr == NULL)
     return NULL;
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCAlloc(ptr, count * size);
   }
 
@@ -87,22 +88,17 @@ static void* tracy_calloc(size_t count, size_t size)
 
 static void* tracy_realloc(void* ptr, size_t new_size)
 {
-  static void* (*next)(void*, size_t) = NULL;
-  if tracy_unlikely(!next) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "realloc");
-    assert(next != NULL);
-    assert(next != &tracy_realloc);
-  }
+  DLSYM_REAL(realloc);
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCFree(ptr);
   }
 
-  void* new_ptr = next(ptr, new_size);
+  void* new_ptr = real_realloc(ptr, new_size);
   if (new_ptr == NULL)
     return NULL;
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCAlloc(new_ptr, new_size);
   }
 
@@ -115,18 +111,13 @@ static void* tracy_realloc(void* ptr, size_t new_size)
 
 static void tracy_free(void *ptr)
 {
-  static void (*next)(void*) = NULL;
-  if tracy_unlikely(next == NULL) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "free");
-    assert(next != NULL);
-    assert(next != &tracy_free);
-  }
+  DLSYM_REAL(free);
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCFree(ptr);
   }
 
-  next(ptr);
+  real_free(ptr);
 }
 
 // void* tracy_valloc(size_t size)
@@ -141,40 +132,15 @@ static void tracy_free(void *ptr)
 // {
 // }
 
-static void* tracy__aligned_alloc(size_t alignment, size_t size)
-{
-  static void* (*next)(size_t, size_t) = NULL;
-  if tracy_unlikely(next == NULL) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "_aligned_alloc");
-    assert(next != NULL);
-    assert(next != &tracy__aligned_alloc);
-  }
-
-  void* ptr = next(alignment, size);
-  if (ptr == NULL)
-    return NULL;
-
-  if tracy_likely(TracyCIsStarted) {
-    TracyCAlloc(ptr, size);
-  }
-
-  return ptr;
-}
-
 static void* tracy_memalign(size_t alignment, size_t size)
 {
-  static void* (*next)(size_t, size_t) = NULL;
-  if tracy_unlikely(next == NULL) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "memalign");
-    assert(next != NULL);
-    assert(next != &tracy_memalign);
-  }
+  DLSYM_REAL(memalign);
 
-  void* ptr = next(alignment, size);
+  void* ptr = real_memalign(alignment, size);
   if (ptr == NULL)
     return NULL;
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCAlloc(ptr, size);
   }
 
@@ -183,18 +149,13 @@ static void* tracy_memalign(size_t alignment, size_t size)
 
 static int tracy_posix_memalign(void** ptr, size_t alignment, size_t size)
 {
-  static int (*next)(void**, size_t, size_t) = NULL;
-  if tracy_unlikely(next == NULL) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "posix_memalign");
-    assert(next != NULL);
-    assert(next != &tracy_posix_memalign);
-  }
+  DLSYM_REAL(posix_memalign);
 
-  int result = next(ptr, alignment, size);
+  int result = real_posix_memalign(ptr, alignment, size);
   if (result != 0)
     return result;
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCAlloc(*ptr, size);
   }
 
@@ -204,18 +165,13 @@ static int tracy_posix_memalign(void** ptr, size_t alignment, size_t size)
 #if !defined(__GLIBC__) || __USE_ISOC11
 static void* tracy_aligned_alloc(size_t alignment, size_t size)
 {
-  static void* (*next)(size_t, size_t) = NULL;
-  if tracy_unlikely(next == NULL) {
-    *(void**) (&next) = dlsym(RTLD_NEXT, "aligned_alloc");
-    assert(next != NULL);
-    assert(next != &tracy_aligned_alloc);
-  }
+  DLSYM_REAL(aligned_alloc);
 
-  void* ptr = next(alignment, size);
+  void* ptr = real_aligned_alloc(alignment, size);
   if (ptr == NULL)
     return NULL;
 
-  if tracy_likely(TracyCIsStarted) {
+  if TRACY_LIKELY(TracyCIsStarted) {
     TracyCAlloc(ptr, size);
   }
 
@@ -249,7 +205,6 @@ void* malloc(size_t size)                                       TRACY_FORWARD1(t
 void* calloc(size_t size, size_t n)                             TRACY_FORWARD2(tracy_calloc, size, n)
 void* realloc(void* ptr, size_t new_size)                       TRACY_FORWARD2(tracy_realloc, ptr, new_size)
 void  free(void* p)                                             TRACY_FORWARD0(tracy_free, p)
-void* _aligned_alloc(size_t alignment, size_t size)             TRACY_FORWARD2(tracy__aligned_alloc, alignment, size)
 void* memalign(size_t alignment, size_t size)                   TRACY_FORWARD2(tracy_memalign, alignment, size)
 int   posix_memalign(void** ptr, size_t alignment, size_t size) TRACY_FORWARD3(tracy_posix_memalign, ptr, alignment, size)
 
