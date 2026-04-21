6abb259561cc1346dcf45449621a790c26c1ca49
Trevor L. McDonell <trevor@passivelogic.com>
2026-04-21 10:00:57 +0200

    fix: move swift_demangle dlsym lookup to initialisation phase

--- Sources/tracy-cbits/tracy-demangle.cpp
+++ Sources/tracy-cbits/tracy-demangle.cpp
@@ -27,6 +27,7 @@
 
 constexpr size_t ___tracy_demangle_buffer_len = 1024*1024;
 char* ___tracy_demangle_buffer;
+static char* (*swift_demangle)(const char*, size_t, char*, size_t*, uint32_t) = nullptr;
 
 extern "C" const char* ___tracy_demangle( const char* mangled )
 {
@@ -39,17 +40,8 @@ extern "C" const char* ___tracy_demangle( const char* mangled )
 
   // Swift demangling
   if ( mangled[0] == '$' ) {
-    static bool once = false;
-    static char* (*swift_demangle)(const char*, size_t, char*, size_t*, uint32_t) = nullptr;
-
-    if ( !once && !swift_demangle ) {
-      once = true;
-      *(void**) (&swift_demangle) = dlsym(RTLD_DEFAULT, "swift_demangle");
-    }
-
-    if ( !swift_demangle ) {
+    if ( !swift_demangle )
       return nullptr;
-    }
 
     size_t len = ___tracy_demangle_buffer_len;
     return swift_demangle( mangled, mangled_len, ___tracy_demangle_buffer, &len, 0 );
@@ -67,11 +59,13 @@ extern "C" const char* ___tracy_demangle( const char* mangled )
 void ___tracy_init_demangle_buffer()
 {
     ___tracy_demangle_buffer = (char*)tracy::tracy_malloc( ___tracy_demangle_buffer_len );
+    *(void**)(&swift_demangle) = dlsym(RTLD_DEFAULT, "swift_demangle");
 }
 
 void ___tracy_free_demangle_buffer()
 {
     tracy::tracy_free( ___tracy_demangle_buffer );
+    swift_demangle = nullptr;
 }
 
 #endif
